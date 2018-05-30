839c0a1e9f03d8b856f81964e8b07af0fb3d856b
Sjoerd van der Hoorn <sjoerdvanderhoorn@hotmail.com>
2018-05-30 22:49:38 +0200

    Additional inputs and outputs

--- readme.md
+++ readme.md
@@ -1,9 +1,48 @@
 # Tesseract
 Tesseract.js is a pure Javascript port of the popular Tesseract OCR engine. It performs all OCR tasks locally without requiring a connection to any external service.
+
+Tesseract was originally developed at Hewlett-Packard Laboratories Bristol and at Hewlett-Packard Co, Greeley Colorado between 1985 and 1994, with some more changes made in 1996 to port to Windows, and some C++izing in 1998. In 2005 Tesseract was open sourced by HP. Since 2006 it is developed by Google.
+
+![Tesseract flow](https://github.com/sjoerdvanderhoorn/node-red-contrib-tesseract/tesseract-flow.png)
+
+This [Node-RED implementation of Tesseract.js](https://github.com/sjoerdvanderhoorn/node-red-contrib-tesseract) has been provided by Sjoerd van der Hoorn.
+
 # Input
-Pass in a local filename or a image buffer to `msg.payload`.
+* `msg.payload` - Local filename, URL, or image buffer.
+
 # Output
-Returns a string with recognized characters in `msg.payload`.
+* `msg.payload` - String with recognized text.
+* `msg.tesseract` - Object with recognized text split out per line and word, plus confidence information.
+
+```js
+{
+	text: "Text from image\nSecond line",
+	confidence: 87,
+	lines: 
+	[
+		{
+			text: "Text from image",
+			confidence: 93,
+			words:
+			[
+				{
+					text: "Text",
+					confidence: 97
+				},
+				{
+					...
+				}
+			]
+		},
+		{
+			...
+		}
+	]
+}
+```
+
 # Additional information
-* https://github.com/naptha/tesseract.js
-* http://tesseract.projectnaptha.com/
+* [Tesseract.js GitHub](https://github.com/naptha/tesseract.js)
+* [Tesseract demo](http://tesseract.projectnaptha.com/)
+* [Original Tesseract OCR engine](https://github.com/tesseract-ocr/tesseract)
+* [Language files](https://github.com/naptha/tessdata/tree/gh-pages/3.02)
--- tesseract.js
+++ tesseract.js
@@ -1,29 +1,80 @@
 var Tesseract = require('tesseract.js');
+var request = require('request');
+var fs = require('fs');
 
 module.exports = function(RED)
 {
 	function TesseractNode(config)
 	{
-		RED.nodes.createNode(this,config);
+		RED.nodes.createNode(this, config);
 		var node = this;
 		node.on('input', function(msg)
 		{
+			// Download URL
+			if (/^http(s?):\/\//.test(msg.payload))
+			{
+				node.status({fill: "blue", shape: "dot", text: "downloading image"});
+				request({url:msg.payload, encoding: null}, function(err, res, body)
+				{
+					if (err)
+					{
+						node.error("Encountered error while downloading image file. " + err.message);
+					}
+					msg.payload = body;
+					Recognize(msg);
+				});
+			}
+			// Open file on local file system
+			else if (typeof msg.payload == "string")
+			{
+				if (fs.existsSync(msg.payload))
+				{
+					Recognize(msg);
+				}
+				else
+				{
+					node.error("Referenced image file does not exist.");
+				}
+			}
+			// Buffer
+			else
+			{
+				Recognize(msg);
+			}
+		});
+		function Recognize(msg)
+		{
 			// Update status - Starting
-			node.status({fill:"blue",shape:"dot",text:"performing ocr"});
+			node.status({fill: "blue", shape: "dot", text: "performing ocr"});
 			// Initiate Tesseract.js
 			Tesseract = new Tesseract.create(
 			{
 				langPath: __dirname + "/langs/"
 			});
 			// Perform OCR
-			Tesseract.recognize(msg.payload).then(function(result)
+			Tesseract.recognize(msg.payload, {lang: "eng"}).then(function(result)
 			{
 				msg.payload = result.text;
+				msg.tesseract = 
+				{
+					text: result.text,
+					confidence: result.confidence,
+					lines: result.lines.map(l => l = 
+					{
+						text: l.text,
+						confidence: l.confidence,
+						words: l.words.map(w => w = 
+						{
+							text: w.text,
+							confidence: w.confidence
+						})
+					})
+				};
 				node.send(msg);
 				// Update status - Done
 				node.status({});
 			});
-		});
+		}
 	}
-	RED.nodes.registerType("tesseract",TesseractNode);
+	RED.nodes.registerType("tesseract", TesseractNode);
 }
